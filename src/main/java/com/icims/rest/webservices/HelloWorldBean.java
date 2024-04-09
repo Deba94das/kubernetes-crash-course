d568c060c16296f4b96274d82064181682eb6ee4
ddas <debainfo24@gmail.com>
2024-04-09 17:26:32 +0530

    code added for hello world rest api

---
+++ src/main/java/com/icims/rest/webservices/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.icims.rest.webservices;
+
+public class HelloWorldBean {
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+}
---
+++ src/test/java/com/icims/rest/webservices/helloworldrestapi/HelloWorldRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icims.rest.webservices.helloworldrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
